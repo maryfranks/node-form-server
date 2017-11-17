44841d8c42351fb74aad3f9343c147d2ac995f20
maryfranks <maryfranks@gmail.com>
2017-11-17 09:49:23 -0500

    accept form data

--- formserver.js
+++ formserver.js
@@ -3,9 +3,41 @@ const fs = require("fs");
 
 http.createServer(function(request, response) {
 
-  response.writeHead(200, {"Content-Type": "text/html"});
+  if (request.method === "GET") {
 
-  fs.createReadStream("./public/form.html", "UTF-8").pipe(response);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream("./public/form.html", "UTF-8").pipe(response);
+
+  } else if (request.method === "POST") {
+
+    var body = "";
+
+    request.on("data", function(chunk) {
+      body += chunk;
+    });
+
+    request.on("end", function() {
+
+      response.writeHead(200, {"Content-Type": "text/html"});
+
+      response.end(`
+
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Form Results</title>
+          </head>
+          <body>
+            <h1>Your Form Results</h1>
+            <p>${body}</p>
+          </body>
+        </html>
+
+        `);
+
+    });
+
+  }
 
 }).listen(3000);
 
