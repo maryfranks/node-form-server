cd8bf85da6be16af3eeb31d1ba74ac14fe58ea32
maryfranks <maryfranks@gmail.com>
2017-11-17 09:34:28 -0500

    display form on page

--- formserver.js
+++ formserver.js
@@ -2,8 +2,11 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World");
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+
+  fs.createReadStream("./public/form.html", "UTF-8").pipe(response);
+
 }).listen(3000);
 
 console.log("listening on port 3000...");
